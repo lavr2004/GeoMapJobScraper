ca349cda4e94018713dceaf4a9888987172f90fe
Andrei Ihnatovicz <lavr2004@gmail.com>
2025-08-16 16:47:18 +0200

    20250815 - UPGRADE: REFACTORING ARCHITECTURE and pipeline process START

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from script00_00_docker_launch import main as step00_prepare_environment_before_launch
+from script00_00_launch_docker import main as step00_prepare_environment_before_launch
 step00_prepare_environment_before_launch()
 
-from script00_00_main import main as step01_parsing_data
+from script00_00_launch_parsers import main as step01_parsing_data
 step01_parsing_data()
 
-from script00_01_combine_databases import combine_databases as step02_combine_data_from_different_databases
+from script00_01_launch_combinedatabases import combine_databases as step02_combine_data_from_different_databases
 step02_combine_data_from_different_databases()
 
-import script00_02_mappagegenerator
+import script00_03_launch_mappagegenerator
 
 print(f"OK - process of parsing, evidence, combining data and generation map finished...")
